0867ef991de653372f49d0058d4faaa75c1c37e4
Aravinda Prasad <aravinda@linux.vnet.ibm.com>
2017-02-19 23:12:51 +0530

    More lists

--- mm/shmem.c
+++ mm/shmem.c
@@ -3121,7 +3121,7 @@ static struct kmem_cache *shmem_inode_cachep;
 static struct inode *shmem_alloc_inode(struct super_block *sb)
 {
 	struct shmem_inode_info *info;
-	info = kmem_cache_alloc(shmem_inode_cachep, GFP_KERNEL);
+	info = kmem_cache_alloc_def(shmem_inode_cachep, GFP_KERNEL);
 	if (!info)
 		return NULL;
 	return &info->vfs_inode;
